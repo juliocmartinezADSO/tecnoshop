340a40fae1e4553b0c3497796220aa3f1ceeb791
juliocmartinezADSO <julio.martinezg8@gmail.com>
2023-05-19 15:24:36 -0500

    Esqueleto de la tienda virtual

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Fragment } from "react";
+import "./menu.css";
+import logo from "../assets/logo.png";
+import carrito from "../assets/Carrito.png";
+import font from "../assets/font.jpg"
+const Menu = () => {
+  return (
+    <Fragment>
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="menu">
+        <div className="menu-top">
+          <div className="text-header">
+            <h2>TecnologyShop</h2>
+          </div>
+          <div className="search-cart">
+            <div className="search-bar">
+              <input type="text" placeholder="Buscar" />
+              <button>Buscar</button>
+            </div>
+            <div className="cart-icon">
+              <a href="#" className="carrito">
+                <img src={carrito} alt="Carrito" />
+                <p>Carrito</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <hr className="line" />
+        <div className="menu-up">
+          <div className="menu-items">
+            <ul>
+              <li>
+                <a href="/">Todo</a>
+              </li>
+              <li>
+                <a href="/">Inicio</a>
+              </li>
+              <li>
+                <a href="/">Catálogo</a>
+              </li>
+              <li>
+                <a href="/">Promociones</a>
+              </li>
+              <li>
+                <a href="/">Contáctenos</a>
+              </li>
+            </ul>
+          </div>
+          <div className="user-auth">
+            <a href="/">Ingresar</a>
+            <a href="/">Registrarse</a>
+          </div>
+        </div>
+      </div>
+      <div className="body">
+
+      </div>
+    </Fragment>
+  );
+};
+
+export default Menu;
